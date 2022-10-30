a301ada26e75de433d701cc7df062a904f4ba193
elimerl <32183908+elimerl@users.noreply.github.com>
2022-10-30 15:19:16 -0700

    "idiomatic" rust

--- src/entities.rs
+++ src/entities.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::{collections::HashMap, fmt::Debug};
 
 use downcast_rs::{impl_downcast, Downcast};
 use glam::{vec3, Vec3};
@@ -10,17 +10,59 @@ fn main() {
     let entity = Entity::new().with_physics(Some(PhysicsObject {
         velocity: Vec3::ZERO,
     }));
+    let entity_id = entity.id;
 
-    let mut entities = vec![entity];
+    let mut world = World::new();
+    world.add_entity(entity);
 
-    gravity_system(&mut entities, 1.0);
+    let mut events = Vec::new();
+
+    println!("before: {:?}", world.entities.get(&entity_id).unwrap());
+    for tick in 0..=1 {
+        events.append(&mut gravity_system(&mut world.entities, 1.0));
+        events.append(&mut physics_system(&mut world.entities, 1.0));
+        world.apply_events(&mut events).unwrap();
+
+        println!(
+            "tick {}:  {:?}",
+            tick,
+            world.entities.get(&entity_id).unwrap()
+        );
+    }
+}
+
+struct World {
+    pub entities: HashMap<Uuid, Entity>,
+}
+impl World {
+    fn new() -> Self {
+        Self {
+            entities: HashMap::new(),
+        }
+    }
+    fn add_entity(&mut self, entity: Entity) {
+        self.entities.insert(entity.id, entity);
+    }
+    fn apply_events(&mut self, events: &mut Vec<Event>) -> Result<(), EventApplyError> {
+        for event in events.drain(..) {
+            match event {
+                Event::Entity(id, event) => self
+                    .entities
+                    .get_mut(&id)
+                    .ok_or(EventApplyError::EntityNotFound)?
+                    .apply_event(event)?,
+            }
+        }
+
+        Ok(())
+    }
 }
 
 pub trait HasID {
     fn id(&self) -> Uuid;
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Clone)]
 struct Entity {
     id: Uuid,
     transform: Transform,
@@ -60,6 +102,9 @@ impl Entity {
 
 #[derive(Error, Debug)]
 pub enum EventApplyError {
+    #[error("an entity was not found")]
+    EntityNotFound,
+
     #[error("event needs physicsobject but it is None")]
     NoPhysics,
 }
@@ -68,13 +113,13 @@ pub trait Component: Downcast + Debug {}
 
 impl_downcast!(Component);
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 struct PhysicsObject {
     pub velocity: Vec3,
 }
 impl Component for PhysicsObject {}
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 struct Transform {
     pub position: Vec3,
 }
@@ -89,26 +134,41 @@ impl Event {
         Self::Entity(entity, event)
     }
 }
+
+#[derive(Clone, Debug)]
 pub enum EntityEvent {
     PositionChange { to: Vec3 },
     VelocityChange { to: Vec3 },
 }
 
-fn gravity_system(entities: &mut [Entity], delta: f32) -> Vec<Event> {
+fn gravity_system(entities: &mut HashMap<Uuid, Entity>, delta: f32) -> Vec<Event> {
     entities
         .par_iter_mut()
-        .filter_map(|v| {
-            if let Some(physics) = &mut v.physics {
-                Some((physics, v.id))
-            } else {
-                None
-            }
-        })
+        .filter_map(|(id, v)| v.physics.as_mut().map(|physics| (physics, *id)))
         .map(|(physics, id)| {
             Event::entity(
                 id,
                 EntityEvent::VelocityChange {
                     to: physics.velocity + vec3(0., -9.81 * delta, 0.),
+                },
+            )
+        })
+        .collect()
+}
+
+fn physics_system(entities: &mut HashMap<Uuid, Entity>, delta: f32) -> Vec<Event> {
+    entities
+        .par_iter_mut()
+        .filter_map(|(id, v)| {
+            v.physics
+                .as_mut()
+                .map(|physics| (physics, &mut v.transform, *id))
+        })
+        .map(|(physics, transform, id)| {
+            Event::entity(
+                id,
+                EntityEvent::PositionChange {
+                    to: transform.position + (physics.velocity * delta),
                 },
             )
         })
